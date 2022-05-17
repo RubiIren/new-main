acb479826cfa131f2994c0c145bed73b3fcb809b
Irina Rubinshtein <rubinshteiniv@gmail.com>
2022-05-17 20:36:53 +0300

    HW-10 Tag

--- src/test/java/quru/qa/tests/FormTests.java
+++ src/test/java/quru/qa/tests/FormTests.java
@@ -1,18 +1,14 @@
 package quru.qa.tests;
 
-import com.codeborne.selenide.Configuration;
-import com.github.javafaker.Faker;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import quru.qa.pages.RegistrationPage;
-import quru.qa.pages.data.DataFaker;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
-import static java.lang.String.format;
 
+@Tag("demoqa")
 public class FormTests extends TestBase {
 
 
